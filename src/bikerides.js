b243e3c85c21358d6be5e1af77ae743546b81c93
jjweis <john@jjweis.com>
2018-05-10 10:38:00 -0400

    Update bikerides.js

--- src/bikerides.js
+++ src/bikerides.js
@@ -14,7 +14,8 @@ var bikeRides = [
   {date: new Date(2018,4,2) , count: 1 },
   {date: new Date(2018,4,4) , count: 1 },
   {date: new Date(2018,4,7) , count: 1 },
-    {date: new Date(2018,4,8) , count: 1 }
+    {date: new Date(2018,4,8) , count: 1 },
+  {date: new Date(2018,4,10) , count: 1 }
 
 ];
 
