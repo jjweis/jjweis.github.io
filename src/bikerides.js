260bc33df24b93b63fdd299e981d6a8912a69715
jjweis <john@jjweis.com>
2018-09-05 13:58:41 -0400

    Update bikerides.js

--- src/bikerides.js
+++ src/bikerides.js
@@ -58,7 +58,9 @@ var bikeRides = [
   {date: new Date(2018,7,17), count: 1 },
   {date: new Date(2018,7,22), count: 1 },
   {date: new Date(2018,7,23), count: 1 },
-  {date: new Date(2018,7,24), count: 1 }
+  {date: new Date(2018,7,24), count: 1 },
+  {date: new Date(2018,8,4), count: 1 },
+  {date: new Date(2018,8,5), count: 1 }
 ];
 
 var rideChart = calendarHeatmap()
